cbe84bc55556e7542c9f23d66505aebb8eff054c
karel <kmarreroe86@gmail.com>
2019-03-20 23:09:44 -0500

    Static filtering

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/filtering/FilteringController.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/filtering/FilteringController.java
@@ -3,15 +3,26 @@ package com.in28minutes.rest.webservices.restfulwebservices.filtering;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 
 @RestController
 public class FilteringController {
 	
 	@GetMapping("/filtering")
-	public SomeBean retrieveSomeBean() {
-		return new SomeBean("value1", "values2", "value3");
+	public MappingJacksonValue retrieveSomeBean() {
+		SomeBean beanVal = new SomeBean("value1", "values2", "value3");
+		SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field1", "field3");
+		FilterProvider filterProvider = new SimpleFilterProvider().addFilter("SimpleBeanFilter", filter);
+		
+		MappingJacksonValue jacksonValue = new MappingJacksonValue(beanVal);
+		jacksonValue.setFilters(filterProvider);
+		return jacksonValue;
 	}
 
 	@GetMapping("/filtering-list")
